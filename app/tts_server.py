e2a782dc8c7b61ab6a8315f6c807fbee9d095f05
Alex Ma 天一 <alexalternative710@gmail.com>
2026-01-11 19:16:01 -0700

    init

---
+++ app/tts_server.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from supertonic import TTS
+import uuid
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+app.mount("/audio", StaticFiles(directory="."), name="audio")
+
+class TTSRequest(BaseModel):
+    text: str
+
+@app.post("/tts")  # <--- must be POST
+def tts(req: TTSRequest):
+    tts = TTS(auto_download=True)
+
+    # Get a voice style
+    style = tts.get_voice_style(voice_name="M1")
+
+    # Generate speech
+    text = req.text
+    wav, duration = tts.synthesize(text, voice_style=style)
+
+    # Save to file
+    filename = f"audio_{uuid.uuid4().hex}.wav"
+    tts.save_audio(wav, f"audios/{filename}")
+
+    return {"audio_file": filename}
